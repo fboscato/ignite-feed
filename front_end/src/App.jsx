8f5c4f66e80cd63589222056b1ca629e869ca36f
fboscato <fboscato@gmail.com>
2023-03-01 17:57:17 -0300

    sidebar

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,16 +1,23 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 export function App() {
   return (
-    <>
+    <div>
       <Header />
-      <Post
-        author='Fernando Boscato'
-        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, sunt! Nesciunt officiis possimus nihil ea voluptatibus a corrupti! Necessitatibus nostrum fugit repellat nobis doloribus, similique deleniti mollitia? Error, deserunt dolor!'
-      />
-      <Post author='Diego Fernandes' content='Novo Post' />
-      <Post />
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Fernando Boscato'
+            content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, sunt! Nesciunt officiis possimus nihil ea voluptatibus a corrupti! Necessitatibus nostrum fugit repellat nobis doloribus, similique deleniti mollitia? Error, deserunt dolor!'
+          />
+          <Post author='Diego Fernandes' content='Novo Post' />
+          <Post />
+        </main>
+      </div>
+    </div>
   );
 }
--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ingite Feed</strong>
+      <img src={igniteLogo} alt='Logo' />
     </header>
   );
 }
