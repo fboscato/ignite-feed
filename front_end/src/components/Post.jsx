11fe860336a77872dc8fdc80851ee8e180a369d9
fernandoboscato <fernandoboscato@faee.com.br>
2023-03-27 16:51:26 -0300

    Post

--- front_end/src/components/Post.jsx
+++ front_end/src/components/Post.jsx
@@ -1,20 +1,31 @@
 import styles from './Post.module.css';
+
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <dir className={styles.author}>
-          <img src='https://github.com/fboscato.png' />
+          <img className={styles.avatar} src='https://github.com/fboscato.png' />
           <div className={styles.authorInfo}>
             <strong>Fernando Boscato</strong>
             <span>Web Developer</span>
           </div>
         </dir>
-        <time title='11 marco 2023' dateTime='2023-03-02 08:14:30'>
+
+        <time title='11 marco 2023 às 08:14' dateTime='2023-03-02 08:14:30'>
           Publicado a 1h
         </time>
       </header>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀        </p>
+        <p>  👉 jane.design/doctorcare        </p>
+        <p> 
+          <a href="">#novoprojeto</a> {' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+        </div>
     </article>
   );
 }
